a46b9a00f67034a0d61f560469f59c42a6936b2d
Shrobon Biswas <shrobon.biswas@gmail.com>
2016-10-10 02:06:12 -0600

    First VR Unity App - Trial 1

---
+++ Assets/Scripts/spin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+
+	// Use this for initialization
+	public float degreesPerSecond;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime*degreesPerSecond);
+	}
+}
